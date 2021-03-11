35b8ff9bc0767e36d45e656d6ab56b6490f6d1b4
ROBLOX Team Foundation <github@roblox.com>
2021-03-11 00:22:04 +0000

    Models for v2/persistence

---
+++ ApiSites/Roblox.GamePersistence.Api/Roblox.GamePersistence.Api/Models/V2/UpdateDatastoreRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Roblox.GamePersistence.Api.Models.V2
+{
+    public class UpdateDatastoreRequest
+    {
+        public bool versioningEnabled { get; set; }
+    }
+}
